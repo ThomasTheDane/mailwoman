0565ae83b650cb0af0fa6cbd1b7ce1efd60b8bdc
Thomas Nattestad <thomas.nattestad@gmail.com>
2014-10-13 13:18:45 -0400

    text analysis

--- thomasPlay.py
+++ thomasPlay.py
@@ -1,16 +1,65 @@
-class email(Object):
-	def __init__(self, sender, receiver, date, subject, text):
+from bs4 import BeautifulSoup
+
+class Person(object):
+	def __init__(firstName, lastName, email):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.email = email
+		self.emails = list()
+	
+	def addEmail(email):
+		self.emails += email
+
+class Date(object):
+	def __init__(day, month, year, dayOfWeek):
+		self.day = day
+		self.month = month
+		self.year = year
+		self.dayOfWeek = dayOfWeek
+
+class Email(object):
+	def __init__(self, sender=None, date=None, subject=None, text=None):
 		self.sender = sender
-		self.receiver = receiver
 		self.date = date
 		self.subject = subject
-		self.text = text;
+		self.text = text
 
-def cleanData():
-	
+def cleanData(fileName):
+	emails = list()
+	people = list()
+
+	didEmailEnd = False
+	# for myLine in open("october2014.txt", 'r'):
+
+	myFile = open('october2014.txt', 'r')
+
+	while(True):
+		myLine = myFile.readline()
+		if(myLine == ""):
+			break
+			
+		if myLine[0:5] == "From:":
+			# Start analysizing new email
+			wasLastLineFrom = False
+
+			newEmail = Email(sender = myLine)
+			# dateString = my
+
+			isReadingNewEmail = True
+			while(isReadingNewEmail):
+				emailLine = myFile.readline()
+				print emailLine
+				if("-- next part ---" in myLine or myLine != ""):	
+					isReadingNewEmail = False
+
+
+	# print  data
+
+	# soup = BeautifulSoup(data)
+	# print(soup.get_text())
 
 def main():
-
+	cleanData("octover2014.txt")
 
 
 if __name__ == '__main__':
