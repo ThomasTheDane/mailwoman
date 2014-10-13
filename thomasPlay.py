9009ccd62f6ac250449be23410b816c0a7588203
Thomas Nattestad <thomas.nattestad@gmail.com>
2014-10-13 11:48:19 -0400

    setup and text

---
+++ thomasPlay.py
@@ -0,0 +1,17 @@
+class email(Object):
+	def __init__(self, sender, receiver, date, subject, text):
+		self.sender = sender
+		self.receiver = receiver
+		self.date = date
+		self.subject = subject
+		self.text = text;
+
+def cleanData():
+	
+
+def main():
+
+
+
+if __name__ == '__main__':
+	main()
